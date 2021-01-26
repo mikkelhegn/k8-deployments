5325528ffed26558cf3793e8bb8b57e2d5f8ce8e
Mikkel Mork Hegnhoj <mikhegn@microsoft.com>
2021-01-26 12:53:32 +0100

    updatas

--- Create_cluster/scripts/create_cluster.sh
+++ Create_cluster/scripts/create_cluster.sh
@@ -20,7 +20,11 @@ WIN_POOL_NAME=winp1
 # Getting latest patch of given minor version
 if [ $K8VERSION == 0 ]
 then
+<<<<<<< HEAD
     MINOR_VERSION=1.18
+=======
+    MINOR_VERSION=1.17
+>>>>>>> updatas
     K8VERSION=$(az aks get-versions -l $LOCATION --query "orchestrators[?contains(orchestratorVersion,'$MINOR_VERSION')].orchestratorVersion | [-1]" --output tsv)
 fi
 echo "Using version $K8VERSION"
@@ -49,7 +53,7 @@ az aks create -g $RESOURCE_GROUP --name $CLUSTER_NAME \
 # Adding a Windows nodepool to the cluster
 # --os-type Windows to indicate the OS type for the node pool (linux or windows)
 # --node-count 3 --node-vm-size Standard_D3_v2 nu,ber of nodes and SKU for the node pool
-# echo "Adding Windows node pool $WIN_POOL_NAME..."
+#echo "Adding Windows node pool $WIN_POOL_NAME..."
 #az aks nodepool add -g $RESOURCE_GROUP --cluster-name $CLUSTER_NAME \
 #    --os-type Windows --name $WIN_POOL_NAME --node-count 2 \
 #    --kubernetes-version $K8VERSION
@@ -60,8 +64,8 @@ az aks get-credentials -n $CLUSTER_NAME -g $RESOURCE_GROUP
 echo "Setting context to $CLUSTER_NAME..."
 kubectl config use-context $CLUSTER_NAME
 
-echo -e "\e[0mAdding taints..."
-kubectl get nodes -l beta.kubernetes.io/os=windows -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' \
-    | xargs -I XX k taint nodes XX windows=true:NoSchedule
+# echo -e "\e[0mAdding taints..."
+# kubectl get nodes -l beta.kubernetes.io/os=windows -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' \
+#    | xargs -I XX k taint nodes XX windows=true:NoSchedule
 
 echo 'Done!'
---
+++ vk-vn/vn-storage-mount.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Change these four parameters as needed for your own environment
+AKS_PERS_STORAGE_ACCOUNT_NAME=mystorageaccount$RANDOM
+AKS_PERS_RESOURCE_GROUP=myShare
+AKS_PERS_LOCATION=westeurope
+AKS_PERS_SHARE_NAME=akssharewe
+
+# Create a resource group
+az group create --name $AKS_PERS_RESOURCE_GROUP --location $AKS_PERS_LOCATION
+
+# Create a storage account
+az storage account create -n $AKS_PERS_STORAGE_ACCOUNT_NAME -g $AKS_PERS_RESOURCE_GROUP -l $AKS_PERS_LOCATION --sku Standard_LRS
+
+# Export the connection string as an environment variable, this is used when creating the Azure file share
+export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string -n $AKS_PERS_STORAGE_ACCOUNT_NAME -g $AKS_PERS_RESOURCE_GROUP -o tsv)
+
+# Create the file share
+az storage share create -n $AKS_PERS_SHARE_NAME --connection-string $AZURE_STORAGE_CONNECTION_STRING
+
+# Get storage account key
+STORAGE_KEY=$(az storage account keys list --resource-group $AKS_PERS_RESOURCE_GROUP --account-name $AKS_PERS_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
+
+# Echo storage account name and key
+echo Storage account name: $AKS_PERS_STORAGE_ACCOUNT_NAME
+echo Storage account key: $STORAGE_KEY
+
+kubectl create secret generic azurefile-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY
+
+kubectl apply -f vn-storage-mount.yaml
